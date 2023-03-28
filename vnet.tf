41cb4c7d2bffe5b7384272e301d3ad58fb7205ac
mahg0509 <mahg0509@outlook.com>
2023-03-28 22:01:23 +0530

    Added New VNet

--- vnet.tf
+++ vnet.tf
@@ -9,6 +9,17 @@ resource "azurerm_virtual_network" "demovnet" {
 }
 
 
+resource "azurerm_virtual_network" "demovnet2" {
+  name                = "demovnet2"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = [var.vnet2_cidr]
+  tags = {
+    "env" = var.env
+  }
+}
+
+
 
 
 
