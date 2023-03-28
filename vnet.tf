56ed55d302265ec4d3e87be11555e0795cb1dcda
mahg0509 <mahg0509@outlook.com>
2023-03-28 22:35:20 +0530

    Added Subnet

--- vnet.tf
+++ vnet.tf
@@ -20,6 +20,20 @@ resource "azurerm_virtual_network" "demovnet2" {
 }
 
 
+resource "azurerm_subnet" "demosubnet1" {
+  name                 = "demosubnet1"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.demovnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "demosubnet2" {
+  name                 = "demosubnet2"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.demovnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 
 
 
