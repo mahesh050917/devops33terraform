4ab1ebf5c8e1bade4e73f83a18f009bf43e20221
Ubuntu <ubuntu@ip-172-31-56-50.ec2.internal>
2023-03-28 12:59:48 +0000

    third commit

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-rgname   = "testrg"
-location = "eastus2"
-env      = "dev" 
+rgname     = "testrg"
+location   = "eastus2"
+env        = "dev"
 vnet1_cidr = "10.0.0.0/16"
--- vnet.tf
+++ vnet.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_network" "demovnet" {
   name                = "demovnet"
-  location            = azurerm_resource_group.rg1.name
-  resource_group_name = azurerm_resource_group.rg1.location
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
   address_space       = [var.vnet1_cidr]
   tags = {
-     "env" = var.env
- }
+    "env" = var.env
+  }
 }
 
 
